e5b315e7fe78c08bcde0d6f8e145122b30d2bd32
Victor Hugo F. Francheto <victor.h.f.francheto@gmail.com>
2025-06-30 21:29:39 -0300

    Delete challenge-main directory

--- challenge-main/src/prevision_falla/main.py
+++
@@ -1,8 +0,0 @@
-
-
-# main.py
-from failure import FailurePredictionPipeline
-
-if __name__ == '__main__':
-    pipeline = FailurePredictionPipeline(file_name='full_devices.csv')
-    pipeline.run()
